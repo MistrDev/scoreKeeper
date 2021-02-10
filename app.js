eb8e30f1bd33f57459e394dd5a22552561bf8617
MistrDev <cwcorster123@gmail.com>
2021-02-10 09:33:55 -0600

    made the buttons have logic

--- app.js
+++ app.js
@@ -5,13 +5,18 @@ const p2Display = document.querySelector('#p2Display')
 
 let p1Score = 0;
 let p2Score = 0;
+let winningScore = 5;
 
 p1Button.addEventListener('click' , function() {
+    if(p1Score !== winningScore){
     p1Score += 1;
     p1Display.textContent = p1Score;
+    }
 })
 
 p2Button.addEventListener('click' , function() {
+    if(p2Score !== winningScore){
     p2Score += 1;
     p2Display.textContent = p2Score;
+    }
 })
